fc1631366c6db7f9774aa9dcba1dc109ad3e0911
Aria <arya_math@yahoo.com>
2025-10-09 00:35:52 +0330

    initial commit

---
+++ app.js
@@ -0,0 +1,65 @@
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+
+require('dotenv').config();
+require('express-async-errors');
+const express = require('express');
+const app = express();
+const authenticateUser = require('./middleware/authentication.js')
+
+
+// connectDB
+const connectDB = require('./db/connect.js')
+
+// routers
+const authRouter = require('./routes/auth.js');
+const jobsRouter = require('./routes/jobs.js');
+
+// error handler
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+
+
+app.use(express.json());
+
+
+// extra packages
+app.set('trust proxy', 1);
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  }))
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+app.get('/' , (req , res) =>{
+  res.send('Jobs API')
+})
+
+// routes
+app.use('/api/v1/auth' , authRouter);
+app.use('/api/v1/jobs' , authenticateUser ,  jobsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
